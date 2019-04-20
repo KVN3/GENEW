3ca73dfd9a5e89e9d677c36602d9c1bf0a05d6c7
KEVIN <kevin@qubics.nl>
2019-04-20 02:20:32 +0200

    recovery

---
+++ Assets/SmokeScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeScreen : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(WaitAndDestroy());
+    }
+
+    IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
