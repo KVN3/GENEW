2fc47833e08e4f5f3aa96bf33aadefcb241dc045
KEVIN <kevin@qubics.nl>
2019-04-22 18:49:10 +0200

    main menu update: 2 new backgrounds, button sounds, return to main button, background track, level loading fixed

--- Assets/_Scripts/UI/MainMenuManager.cs
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -6,11 +6,21 @@ using UnityEngine.UI;
 
 public class MainMenuManager : LevelSingleton<MainMenuManager>
 {
+   // [SerializeField]
+    //private MenuSoundManager menuSoundManagerClass;
+
     [Header("Text")]
     public TextMeshProUGUI TitleText;
     public TextMeshProUGUI SingleplayerText;
     public TextMeshProUGUI OptionsText;
     public TextMeshProUGUI VersionText;
+
+    //private MenuSoundManager menuSoundManager;
+
+    new void Awake()
+    {
+       // menuSoundManager = Instantiate(menuSoundManagerClass, transform.localPosition, transform.localRotation, this.transform);
+    }
 
     // Start is called before the first frame update
     void Update()
