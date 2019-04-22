0294b9f72d1858b55664eecdaecc2b588a745e3e
KEVIN <kevin@qubics.nl>
2019-04-22 07:42:22 +0200

    big epic irish AI robot voice implemented

---
+++ Assets/_Scripts/Sound Manager/Sub/AISoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISoundManager : SoundManager
+{
+    public AudioClip[] forcefieldAvailableClips;
+    public AudioClip[] minesAvailableClips;
+    public AudioClip[] projectilesAvailableClips;
+    public AudioClip[] smokescreenAvailableClips;
+    public AudioClip[] speedboostAvailableClips;
+
+    public override void PlaySound(SoundType soundType)
+    {
+        switch (soundType)
+        {
+            case SoundType.AIFORCEFIELD:
+                audioSource.clip = forcefieldAvailableClips[Random.Range(0, forcefieldAvailableClips.Length)];
+                break;
+            case SoundType.AIMINES:
+                audioSource.clip = minesAvailableClips[Random.Range(0, minesAvailableClips.Length)];
+                break;
+            case SoundType.AIPROJECTILES:
+                audioSource.clip = projectilesAvailableClips[Random.Range(0, projectilesAvailableClips.Length)];
+                break;
+            case SoundType.AISMOKESCREEN:
+                audioSource.clip = smokescreenAvailableClips[Random.Range(0, smokescreenAvailableClips.Length)];
+                break;
+            case SoundType.AISPEED:
+                audioSource.clip = speedboostAvailableClips[Random.Range(0, speedboostAvailableClips.Length)];
+                break;
+        }
+
+        audioSource.Play();
+    }
+}
