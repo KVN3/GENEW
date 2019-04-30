0eca93a9d218eac183d5e13e7f1d42408ef03787
KEVIN <kevin@qubics.nl>
2019-04-30 03:43:28 +0200

    Multiplayer scene managing & scenesinformation

--- Assets/_Scripts/mp/Networks/PlayerNetwork.cs
+++ Assets/_Scripts/mp/Networks/PlayerNetwork.cs
@@ -1,15 +1,68 @@
 ﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : MonoBehaviourPunCallbacks
 {
-    public static PlayerNetwork instance;
+    public static PlayerNetwork Instance;
 
     public string PlayerName { get; private set; }
 
+    private PhotonView photonView;
+    private int playersInGame = 0;
+
     private void Awake()
     {
-        instance = this;
+        Instance = this;
+        photonView = GetComponent<PhotonView>();
 
         PlayerName = "Kevin#" + Random.Range(1000, 9999);
+
+        // Delegate, when scene loaded method called
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+    }
+
+
+    private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if (!scene.name.Equals("Main Menu"))
+        {
+            string wasteland = ScenesInformation.sceneNames[SceneTitle.Wasteland];
+
+            if (scene.name.Equals(wasteland))
+            {
+                if (PhotonNetwork.IsMasterClient)
+                    MasterLoadedGame();
+                else
+                    NonMasterLoadedGame();
+            }
+        }
+    }
+
+    private void MasterLoadedGame()
+    {
+        playersInGame = 1;
+        photonView.RPC("RPC_LoadGameOthers", RpcTarget.Others);
+    }
+
+    private void NonMasterLoadedGame()
+    {
+        photonView.RPC("RPC_LoadedGameScene", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    private void RPC_LoadGameOthers()
+    {
+        string wasteland = ScenesInformation.sceneNames[SceneTitle.Wasteland];
+
+        PhotonNetwork.LoadLevel(wasteland);
+    }
+
+    [PunRPC]
+    private void RPC_LoadedGameScene()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            print("All players loaded game scene.");
     }
 }
