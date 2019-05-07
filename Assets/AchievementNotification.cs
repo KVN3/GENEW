c85936a4c540d13730ae9c2314a5a5f96cb10047
Gianni <gianni.stekkinger@hotmail.nl>
2019-05-07 04:27:11 +0200

    Added achievement notifications (needs tweaking)

---
+++ Assets/AchievementNotification.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class AchievementNotification : MonoBehaviour
+{
+    #region Fields
+	public TextMeshProUGUI titleText;
+	#endregion
+	
+	#region Unity Methods
+	public void SetText(Achievement achievement)
+    {
+        titleText.text = achievement.title;
+    }
+	#endregion
+}
