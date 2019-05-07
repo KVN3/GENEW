bfc3d6f0e6503d1f4d03f8799b44a4b513dbdd21
Gianni <gianni.stekkinger@hotmail.nl>
2019-05-07 02:20:38 +0200

    RIP singleplayer, MP = Play, Changelog, MP btn sounds

--- Assets/_Scripts/mp/Lobby/LobbyCanvas.cs
+++ Assets/_Scripts/mp/Lobby/LobbyCanvas.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using TMPro;
 
 public class LobbyCanvas : MonoBehaviourPunCallbacks
 {
+    public TextMeshProUGUI lobbyText;
     public Text createRoomText;
     public Text roomNameText;
     public Text returnText;
@@ -19,6 +21,7 @@ public class LobbyCanvas : MonoBehaviourPunCallbacks
 
     public void Update()
     {
+        lobbyText.text = LocalizationManager.GetTextByKey("LOBBY");
         createRoomText.text = LocalizationManager.GetTextByKey("CREATE_ROOM");
         roomNameText.text = LocalizationManager.GetTextByKey("ROOM_NAME") + "...";
         returnText.text = LocalizationManager.GetTextByKey("RETURN_TO_MAIN_MENU");
