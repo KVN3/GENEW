a01bf97477319606c16f8c5037864d57c0908f7f
Gianni <gianni.stekkinger@hotmail.nl>
2019-05-27 02:23:55 +0200

    Endscreen working, pause screen (p), countdown back + bunch of fixes

---
+++ Assets/PauseScreen.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScreen : MonoBehaviour
+{
+    #region Fields
+    public Button restartBtn;
+	#endregion
+	
+	#region Unity Methods
+	void Start()
+    {
+        //if (PhotonNetwork.PlayerList.Length == 1)
+        //    restartBtn.gameObject.SetActive(true);
+    }
+	#endregion
+}
